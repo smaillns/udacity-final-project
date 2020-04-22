65d04692c93e847df5a562f0e92b8f46380113e0
Smail LOUNES <slounes@wissal-sys.com>
2020-04-22 02:29:43 +0100

    fix deployment issue

--- enterprice/src/environments/environment.prod.ts
+++ enterprice/src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'smaillns.auth0.com',            // Auth0 domain
   clientId: 'EsB8Mm1tOBmpXvXk23bNfgkGZ62GTSS6',          // Auth0 client id
-  callbackUrl: 'http://www.ucci.uk/callback'
+  callbackUrl: 'http://www.ucci.uk/client'
 }
